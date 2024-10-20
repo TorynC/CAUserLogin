872fe561a9327900a4d877c3bfaf47c23ae8b96f
Toryn <torync03@gmail.com>
2024-10-20 15:00:44 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     * @param name the name that is needed.
+     */
+    void setCurrentUser(String name);
 }
